645ff42773079044daa2890b783e42896c68d5d4
Scott Chen <yanbin.chen.cn@gmail.com>
2024-03-03 19:02:34 +0800

    add doc string with sections (code block section)

--- 06_structure/src/product.rs
+++ 06_structure/src/product.rs
@@ -3,6 +3,7 @@ pub mod category;
 use crate::product::category::Category;
 
 #[derive(PartialEq, Debug)]
+/// Represents a product.
 pub struct Product {
     id: u32,
     name: String,
@@ -11,6 +12,18 @@ pub struct Product {
 }
 
 impl Product {
+    /// Creates a new product with the specified id, name, price, and category.
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The unique identifier of the product.
+    /// * `name` - The name of the product.
+    /// * `price` - The price of the product.
+    /// * `category` - The category of the product.
+    ///
+    /// # Returns
+    ///
+    /// A new `Product` instance.
     pub fn new(id: u32, name: String, price: f64, category: Category) -> Product {
         Product {
             id,
@@ -20,7 +33,12 @@ impl Product {
         }
     }
 
-    fn caculate_tax(&self) -> f64 {
+    /// Calculates the tax amount based on the product's category.
+    ///
+    /// # Returns
+    ///
+    /// The tax amount.
+    fn calculate_tax(&self) -> f64 {
         match self.category {
             Category::Clothing => self.price * 0.05,
             Category::Electronics => self.price * 0.18,
@@ -29,7 +47,23 @@ impl Product {
         }
     }
 
+    /// Calculates the total price of the product including tax.
+    ///
+    /// # Returns
+    ///
+    /// The total price of the product.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use structure_test::Category;
+    /// use structure_test::Product;
+    ///
+    /// let product = Product::new(1, "Shirt".to_string(), 20.0, Category::Clothing);
+    /// let total_price = product.calculate_product_price();
+    /// assert_eq!(total_price, 21.0);
+    /// ```
     pub fn calculate_product_price(&self) -> f64 {
-        self.price + self.caculate_tax()
+        self.price + self.calculate_tax()
     }
 }
--- 06_structure/src/product/category.rs
+++ 06_structure/src/product/category.rs
@@ -1,5 +1,7 @@
 #[derive(PartialEq, Debug)]
+/// Category of the product
 pub enum Category {
+    /// Clothing category
     Clothing,
     Electronics,
     Grocery,
