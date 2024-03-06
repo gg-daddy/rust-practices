95d3b5c8286158c63a9371bb0daf1272f648a33f
Scott Chen <yanbin.chen.cn@gmail.com>
2024-03-06 20:07:27 +0800

    add FnMut Example

---
+++ 11_functional/src/bin/event_handler.rs
@@ -0,0 +1,46 @@
+// Problem 4: Fix the struct definition to allow closures with event handling logic.
+
+struct EventHandler<T>
+where
+    T: FnMut() -> (), // Something wrong here.
+             //Hint: Check the code in main and see how the closure is using
+             // the values from its enviroment
+{
+    on_event: T,
+}
+
+impl<T> EventHandler<T>
+where
+    T: FnMut() -> (), // Something wrong here
+{
+    fn handle_event(&mut self) {
+        (self.on_event)()                                                                           
+    }
+}
+
+fn main() {
+    let mut lights_on = false;
+    let mut temperature = 25;
+
+    let mut lights_handler = EventHandler {
+        on_event: || {
+            lights_on = !lights_on;
+            println!("Lights are now {}", if lights_on { "on" } else { "off" });
+        },
+    };
+
+    let mut temperature_handler = EventHandler {
+        on_event: || {
+            temperature += 5;
+            println!("Temperature increased to {}°C", temperature);
+        },
+    };
+
+    lights_handler.handle_event();
+    temperature_handler.handle_event();
+    temperature_handler.handle_event();
+    lights_handler.handle_event();
+
+    assert_eq!(temperature, 35);
+    assert_eq!(lights_on, true);
+}
